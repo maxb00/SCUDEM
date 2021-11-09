279f2c5142464d8b5d17184be13d1b33e1dfa58c
Max Barlow <barlowm15@gmail.com>
2021-11-09 15:26:52 -0500

    create functions to house data

--- second.py
+++ second.py
@@ -1,10 +1,17 @@
 # File to model and graph a system of differential equations
 # modeling population change in birds who steal hair from live animals.
 from math import cos
+from math import pi as p
 import numpy as np
 import matplotlib.pyplot as plt
 
 
+# globals true for every population
+pi = 0.3 # proportion of normal pop increase
+pd = 0.11 # proportion of normal pop decrease
+
+
+# computational functions (building system)
 def temp(theta, beta):
     ''' Temperature function for the system of differential equations. '''
     return abs(beta * cos(theta))
@@ -35,8 +42,72 @@ def growth(ri, re, pi, pd):
         return pi - pd
 
 
+# Data functions
+def load_live_data():
+    '''Holds data for birds who steal live hairs'''
+    # maximums
+    beta = 0.12
+    att_max = 0.5
+    infect_max = 0.5
+    rho_max = 0.5 # predator constant
+    eta_max = 0.5 # parasite constant
+
+    # actives (max be moved)
+    theta = 0 # range 0 - pi, max at pi/2
+    att = 0.2
+    infect = 0.1
+    rho = 0.07
+    eta = 0.23
+
+    return [beta, att_max, infect_max, rho_max, eta_max], [theta, att, infect, rho, eta]
+
+
+def load_dead_data():
+    '''Holds data for birds who steal live hairs'''
+    # maximums
+    beta = 0.12
+    att_max = 0.5
+    infect_max = 0.5
+    rho_max = 0.5 # predator constant
+    eta_max = 0.5 # parasite constant
+
+    # actives (max be moved)
+    theta = 0 # range 0 - pi, max at pi/2
+    att = 0.1
+    infect = 0.4
+    rho = 0.07
+    eta = 0.16
+
+    return [beta, att_max, infect_max, rho_max, eta_max], [theta, att, infect, rho, eta]
+
+
+def load_nohair_data():
+    '''Holds data for birds who steal live hairs'''
+    # maximums
+    beta = 0.12
+    att_max = 0.5
+    infect_max = 0.5
+    rho_max = 0.5 # predator constant
+    eta_max = 0.5 # parasite constant
+
+    # actives (max be moved)
+    theta = 0 # range 0 - pi, max at pi/2
+    att = 0.001
+    infect = 0.05
+    rho = 0.2
+    eta = 0.4
+
+    return [beta, att_max, infect_max, rho_max, eta_max], [theta, att, infect, rho, eta]
+
+
 def main():
-    pass
+    # initialize populations
+
+    # run years, saving history
+
+    # graph history
+
+    print('This is a placeholder.')
 
 
 if __name__ == '__main__':
