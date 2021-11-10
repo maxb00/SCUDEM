4a3002b203ce45d55f952677c4011eb60a66be99
maxb00 <barlowm15@gmail.com>
2021-11-10 11:24:10 -0500

    simulation setup

--- second.py
+++ second.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 # globals true for every population
 pi = 0.3 # proportion of normal pop increase
 pd = 0.11 # proportion of normal pop decrease
+pop_max = 1000 # population maximum
 
 
 # computational functions (building system)
@@ -63,7 +64,7 @@ def load_live_data():
 
 
 def load_dead_data():
-    '''Holds data for birds who steal live hairs'''
+    '''Holds data for birds who steal dead hairs'''
     # maximums
     beta = 0.12
     att_max = 0.5
@@ -82,7 +83,7 @@ def load_dead_data():
 
 
 def load_nohair_data():
-    '''Holds data for birds who steal live hairs'''
+    '''Holds data for birds who steal no hairs'''
     # maximums
     beta = 0.12
     att_max = 0.5
@@ -102,13 +103,63 @@ def load_nohair_data():
 
 def main():
     # initialize populations
+    pop_live = 100
+    pop_dead = 100
+    pop_nohair = 100
+
+    # initialize history lists
+    live_history = []
+    dead_history = []
+    nohair_history = []
 
     # run years, saving history
+    for _ in range(1, 50): # 100 years
+        # load data
+        [beta, att_max, infect_max, rho_max, eta_max], [theta, att, infect, rho, eta] = load_live_data()
+        # calculate risk and reward
+        risk_live = risk(att, att_max, infect, infect_max)
+        reward_live = reward(rho, rho_max, eta, eta_max, beta, theta)
+        # calculate growth
+        growth_live = growth(risk_live, reward_live, pi, pd)
+        # update population
+        pop_live_change = growth_live * pop_live * (1 - ((pop_live + pop_dead + pop_nohair) / pop_max))
+
+        # load data dead hairs
+        [beta, att_max, infect_max, rho_max, eta_max], [theta, att, infect, rho, eta] = load_dead_data()
+        # calculate risk and reward
+        risk_dead = risk(att, att_max, infect, infect_max)
+        reward_dead = reward(rho, rho_max, eta, eta_max, beta, theta)
+        # calculate growth
+        growth_dead = growth(risk_dead, reward_dead, pi, pd)
+        # update population
+        pop_dead_change = growth_dead * pop_dead * (1 - ((pop_live + pop_dead + pop_nohair) / pop_max))
+
+        # load data no hairs
+        [beta, att_max, infect_max, rho_max, eta_max], [theta, att, infect, rho, eta] = load_nohair_data()
+        # calculate risk and reward
+        risk_nohair = risk(att, att_max, infect, infect_max)
+        reward_nohair = reward(rho, rho_max, eta, eta_max, beta, theta)
+        # calculate growth
+        growth_nohair = growth(risk_nohair, reward_nohair, pi, pd)
+        # update population
+        pop_nohair_change = growth_nohair * pop_nohair * (1 - ((pop_live + pop_dead + pop_nohair) / pop_max))
+
+        # update populations
+        pop_live += pop_live_change
+        pop_dead += pop_dead_change
+        pop_nohair += pop_nohair_change
+
+        # save history
+        live_history.append(pop_live)
+        dead_history.append(pop_dead)
+        nohair_history.append(pop_nohair)
 
     # graph history
-
-    print('This is a placeholder.')
-
+    plt.plot(live_history, label='live')
+    plt.plot(dead_history, label='dead')
+    plt.plot(nohair_history, label='nohair')
+    plt.legend()
+    plt.show()
 
 if __name__ == '__main__':
     main()
